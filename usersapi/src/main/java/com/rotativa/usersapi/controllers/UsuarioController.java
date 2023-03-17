a69beb3759e37520c94a71c36c16e6a9238c30eb
Ezequiel Sousa <ezequielss@unifor.br>
2023-03-17 13:17:45 -0300

    clean code

--- usersapi/src/main/java/com/rotativa/usersapi/controllers/UsuarioController.java
+++ usersapi/src/main/java/com/rotativa/usersapi/controllers/UsuarioController.java
@@ -1,60 +1,54 @@
-package com.rotativa.usersapi.Controllers;
+package com.rotativa.usersapi.controllers;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.rotativa.usersapi.Entidades.UsuarioModel;
-import com.rotativa.usersapi.Services.UsuarioService;
+import com.rotativa.usersapi.model.Usuario;
+import com.rotativa.usersapi.services.UsuarioService;
 
 @CrossOrigin(origins = "http://localhost:3000") 
 @RestController
-@RequestMapping
+@RequestMapping("/usuario")
 public class UsuarioController {
     @Autowired 
     UsuarioService usersService;
 
-    @GetMapping("/usuario")
-    public List<UsuarioModel> listar(){
-        return this.usersService.listar();
+    @GetMapping
+    public List<Usuario> listar(){
+        return this.usersService.findAll();
     }
 
     @PostMapping
-    public void salvar(@RequestBody UsuarioModel usuario){
-        usersService.salvar(usuario);
+    public void salvar(@RequestBody Usuario usuario){
+        usersService.save(usuario);
     }
 
-    @PatchMapping
-    public void alterar(@RequestBody UsuarioModel usuario){
-        usersService.alterar(usuario);
+    @PutMapping
+    public void alterar(@RequestBody Usuario usuario){
+        usersService.partialUpdate(usuario);
     }
 
-    @DeleteMapping
-    public void excluir(@RequestBody UsuarioModel usuario) {
-        usersService.excluir(usuario);
-    }
+    // @GetMapping("/filtro")
+    // public List<Usuario> filtroUsuario(@RequestParam("nome") String nome) {
+    //     System.out.println("Nome = " + nome);
+    //     List<Usuario> usuarios = usersService.carregarPorNome(nome);
+    //     return usuarios;
+    // }
 
-    @GetMapping("/filtro")
-    public List<UsuarioModel> filtroUsuario(@RequestParam("nome") String nome) {
-        System.out.println("Nome = " + nome);
-        List<UsuarioModel> usuarios = usersService.carregarPorNome(nome);
-        return usuarios;
-    }
-
-    @GetMapping("/relatorio")
-    List<List<String>> relatorioUsuario() {
-        List<List<String>> filtro = usersService.relatorioUsuario();
-        return filtro;
-    }
+    // @GetMapping("/relatorioVaga")
+    // List<Usuari> relatorioUsuario(@RequestParam("nome") String nome) {
+    //     List<Usuari> filtro = usersService.getUsuariosVaga(nome);
+    //     return filtro;
+    // }
 
 
 
