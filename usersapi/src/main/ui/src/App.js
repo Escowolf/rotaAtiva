ce516976641563edb9f3fd6dbd063c6816bfe0ca
Ezequiel Escobar <escosou@gmail.com>
2023-11-23 18:16:53 -0300

    remoção do rodape e login do App.jsx

--- usersapi/src/main/ui/src/App.js
+++ usersapi/src/main/ui/src/App.js
@@ -5,9 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Cabecalho } from "./externo/cabecalho/cabecalho";
 import { Route, Routes, useLocation } from "react-router-dom";
 import { Index } from "./externo/index/index";
-import { Rodape } from "./externo/rodape/rodape";
 import { Cadastro } from "./externo/cadastro/cadastro";
-import { Login } from "./externo/login/login";
 import { Map } from "./interno/map/map";
 import { Home } from "./interno/home/Home";
 import { RelatorioVagas } from "./interno/relatorio_vaga/relatorioVaga";
@@ -22,11 +20,10 @@ import { RemoverVaga } from "./interno/adicionar_remover/RemoverVaga";
 function App() {
   return (
     <>
-      {useLocation().pathname == "/" ? <Cabecalho /> : ""}
+      {useLocation().pathname === "/" ? <Cabecalho /> : ""}
       <Routes>
         <Route element={<Index />} exact path="/"></Route>
         <Route element={<Cadastro />} exact path="/cadastro"></Route>
-        <Route element={<Login />} exact path="/login"></Route>
         <Route element={<Home />} exact path="/menulogado">
           <Route element={<Dashboard />} path=":id"/>
           <Route element={<Map />} path="mapa"/>
@@ -39,7 +36,6 @@ function App() {
           <Route element={<RemoverVaga/>} path="removerVaga"/>
         </Route>
       </Routes>
-      {/* {useLocation().pathname == "/" ? <Rodape /> : ""} */}
     </>
   );
 }
